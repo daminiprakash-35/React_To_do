88a12f3ad16e658db349f2fbcbcbfaa730b47b53
daminiprakash-35 <daminiprakash35@gmail.com>
2022-01-23 19:05:14 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React, { useState} from "react";
+import trashIcon from './trash.svg'
+
+
+
+function App() {
+    const [taskInput, updateTaskInput] = useState("");
+    const [toDoList, updateToDOList] = useState([]);
+
+  /*  useEffect(() => {
+        let arr = localStorage.getItem("toDoList")
+        
+        if(arr){
+            let obj = JSON.parse(arr)
+            updateToDOList(obj)
+        }
+    },[])*/
+
+
+
+    const inputKeyDown = (event) => {
+        if (event.keyCode === 13) addNote();
+    };
+    const getTaskObject = (description, isComplete) => {
+        return {
+            description,
+            isComplete
+        }
+    }
+
+    const addNote = () => {
+        if (!taskInput || !taskInput.length)
+            return;
+        toDoList.push(getTaskObject(taskInput, false));
+        //localStorage.setItem("toDoList",JSON.stringify(taskInput))
+        updateToDOList(toDoList);
+        updateTaskInput("")
+    };
+
+    const deleteTask = (index) => {
+        let splice = toDoList.filter((item, i) => i !== index);
+        updateToDOList(splice);
+    }
+
+    const markComplete = (index) => {
+      const list = [...toDoList]
+      list[index].isComplete = !list[index].isComplete
+      updateToDOList(list)
+  }
+
+
+  return (
+    <div className="app-background">
+     <p className="heading-text"> To-do List</p>
+     <div className="task-container column">
+                <div className="row">
+                    <input
+                        className="text-input"
+                        value={taskInput}
+                        onKeyDown={inputKeyDown}
+                        onChange={(event) => updateTaskInput(event.target.value)}
+                    />
+                    <button className="add-button" onClick={addNote}>
+                        ADD
+                    </button>
+                </div>
+                {toDoList?.length ?
+                    toDoList.map((obj, index) =>
+                        <ListItem key={index} itemData={obj} markComplete={markComplete}
+                                  index={index} deleteTask={deleteTask}/>) :
+                    <p className="no-item-text"><span role="img" aria-label="react">📌</span> &nbsp;No Task Added !</p>}
+            </div>
+            <p className="footer-text">copyright@2021</p>
+        </div>
+        
+    );
+}
+
+function ListItem(props) {
+    return (
+        <div className="list-item row jc-space-between">
+            <span className={props.itemData.isComplete ? 'task-complete' : ''}
+                  onClick={() => props.markComplete(props.index)}>{props.itemData.isComplete ? `✅ ` : ''}&nbsp;{props.itemData?.description}</span>
+            <img className="delete-icon" src={trashIcon} alt="delete-task"
+                 onClick={() => props.deleteTask(props.index)}/>
+        </div>
+    );
+}
+
+export default App;
